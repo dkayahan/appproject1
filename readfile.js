63c55b938c295cdb4a2d10a50396eebf796855ab
Dilek <dilek_kayahan@hotmail.com>
2017-05-06 21:54:13 +0300

    Read & parse METU SABANCI into corpus

--- readfile.js
+++ readfile.js
@@ -1,19 +1,59 @@
-function readTreeBank(){
-	var file = "file:///home/dilek/Desktop/boun/nlp/app project1/appproject1/METUSABANCI_treebank.conll"
-	var rawFile = new XMLHttpRequest();
-    	rawFile.open("GET", file, false);
-    	rawFile.onreadystatechange = function(){
-        if(rawFile.readyState === 4){
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                console.log(allText);
+/** 
+ * Reads METU Sabancı treebank asyncly
+ * Returns input as callback
+ */
+function readTreeBank(callback){
+	var file = "file:///home/dilek/Desktop/boun/nlp/appProject1/project1/METUSABANCI_treebank.conll";
+	console.log("Reading treebank from: "+file);
+	var xmlReq = new XMLHttpRequest();
+		xmlReq.open("GET", file, true);
+		xmlReq.onreadystatechange = function(){
+        if(xmlReq.readyState === 4){
+            if(xmlReq.status === 200 || xmlReq.status == 0){
+                callback(xmlReq.responseText);
             }
         }
     }
-    rawFile.send(null);
+	xmlReq.send();
+}
+
+/**
+ * Returns corpus as an array of sentence object = {words: ["word1", "word2", "word3"], tags: ["tag1", "tag2", "tag3"]}
+ * Tokens that starts with "_" is ignored during METU Sabancı parse
+ * @param text = text to be parsed
+ */
+function parseCorpus(text){
+	console.log("Parsing METU SABANCI Treebank into corpus");
+	var sentencesInMETU = text.split("\n\n"); //Divide by empty lines
+	console.log("Number of sentences in METU SABANCI Treebank is: ", sentencesInMETU.length);
+	var corpus = new Array();
+	for(var i = 0; i<sentencesInMETU.length; i++){ //Parse each sentence for word and POS
+		var lineOfSentence = sentencesInMETU[i].split("\n"); //Each line of sentence: Ex/ ﻿1	Peşreve	peşrev	Noun	Noun	A3sg|Pnon|Dat	2	OBJECT	_	_
+		var sentenceOfCorpus = {words:[], tags: [], getSentence: function(){
+			return this.words.join(" ");
+		}, getPOSTag: function(){
+			return this.tags.join(" ");
+		}};
+		for(var j = 0; j<lineOfSentence.length; j++){ //Parse each line of sentence
+			var wordOfLine = lineOfSentence[j].split("	"); //3 spaces to divide into words
+			if(wordOfLine[1] != "_"){ //Ignore words marked with underscore _
+				sentenceOfCorpus.words.push(wordOfLine[1]); //word in sentence
+				sentenceOfCorpus.tags.push(wordOfLine[3]); //POS tag
+			}
+		}
+		corpus.push(sentenceOfCorpus);
+	}
+	console.log("Size of corpus: ", corpus.length);
+	return corpus;
 }
 
 function init(){
-	readTreeBank();
+	readTreeBank(function(response){
+		var corpus = parseCorpus(response);
+		console.log(corpus);
+		console.log(corpus[0].getSentence());
+		console.log(corpus[0].getPOSTag());
+		console.log(corpus[0].words);
+		console.log(corpus[0].tags);
+	});
 }
